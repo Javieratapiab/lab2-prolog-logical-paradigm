bd22308e884ad55ab7f7f83c058a4b60f162d556
javieratapiab <javierafrancisca92@gmail.com>
2020-11-29 23:42:31 -0300

    feat: finaliza predicado answer.

--- main.pl
+++ main.pl
@@ -1,17 +1,18 @@
 % Base de conocimiento
 
 % ------------ UTILS ------------------
-exists(Elem,[H|T]):- Elem = H; exists(Elem,T), !.
+exists(Elem,[H|T]):- Elem = H; exists(Elem,T),!,true.
 
 appendList([],List,List).
 appendList([H|T],L2,[H|List]):- appendList(T,L2,List).
 
-customLast(X,X).
-customLast(X,[_|L]):- customLast(X,L).
+%customLast(X,X).
+%customLast(X,[_|L]):- customLast(X,L).
 
 % ------------ TDA STACK ---------------
 stack([Users,Questions,Answers],[Users,Questions,Answers]).
 question([Id,Author,Date,Text,Labels],[Id,Author,Date,Text,Labels]).
+answer([Id,QuestionId,Author,Date,Text,Labels],[Id,QuestionId,Author,Date,Text,Labels]).
 
 setUsers(Users,[_,Questions,Answers],R):- stack([Users,Questions,Answers],R).
 setQuestions(Questions,[Users,_,Answers],R):- stack([Users,Questions,Answers],R).
@@ -28,8 +29,8 @@ stackRegister([],Username,Password,[[[Username,Password]],[],[]]).
 stackRegister(Stack,Username,Password,Stack2):- getUsers(Stack,Users),
   addUser(Users,Username,Password,U2),
   getQuestions(Stack,Q1),
-  getAnswers(Stack,R1),
-  stack([U2,Q1,R1],Stack2).
+  getAnswers(Stack,A1),
+  stack([U2,Q1,A1],Stack2).
 
 validateRegistedUser(Users,Username,Password,Users):- \+ exists([Username,Password],Users),!,false.
 validateRegistedUser(Users,Username,_,R):- appendList([Username],Users,R).
@@ -37,12 +38,12 @@ validateRegistedUser(Users,Username,_,R):- appendList([Username],Users,R).
 stackLogin(Stack,Username,Password,Stack2):- getUsers(Stack,Users),
   validateRegistedUser(Users,Username,Password,U2),
   getQuestions(Stack,Q1),
-  getAnswers(Stack,R1),
-  stack([U2,Q1,R1],Stack2).
+  getAnswers(Stack,A1),
+  stack([U2,Q1,A1],Stack2).
 
-validateLogguedUser(Users):- getLogguedUser(Users,R), string(R).
-getLogguedUser([H|_], H).
-removeLogguedUser([_|T], T).
+validateLoggedUser(Users):- getLoggedUser(Users,R), string(R).
+getLoggedUser([H|_], H).
+removeLoggedUser([_|T], T).
 
 setQuestionId(Question,NewId):- getQuestionId(Question,Id), NewId is Id + 1.
 getQuestionId([Id,_,_,_,_],Id).
@@ -50,13 +51,36 @@ getQuestionId([Id,_,_,_,_],Id).
 addQuestion([],Author,Date,Text,Labels,[R]):- question([1,Author,Date,Text,Labels],R).
 addQuestion([H|T],Author,Date,Text,Labels,R):- setQuestionId(H,NewId),
     question([NewId, Author,Date,Text,Labels], Q),
-    appendList([Q],[H|T], R).
+    appendList([Q],[H|T],R).
+
+validateQuestionId(Questions,QuestionId):- exists([QuestionId,_,_,_,_],Questions),!,true.
+
+getAnswerId([Id,_,_,_,_,_],Id).
+setAnswerId(Answer,NewId):- getAnswerId(Answer,Id), NewId is Id + 1.
+
+addAnswer([],QuestionId,Author,Date,Text,Labels,[R]):- answer([1,QuestionId,Author,Date,Text,Labels],R).
+addAnswer([H|T],QuestionId,Author,Date,Text,Labels,R):- setAnswerId(H,NewId),
+    answer([NewId,QuestionId,Author,Date,Text,Labels], Q),
+    appendList([Q],[H|T],R).
 
 ask(Stack,Fecha,TextoPregunta,ListaEtiquetas,Stack2):- getUsers(Stack,Users),
-  validateLogguedUser(Users),
-  getLogguedUser(Users,UL),
+  validateLoggedUser(Users),
+  getLoggedUser(Users,UL),
   getQuestions(Stack,Q1),
   getAnswers(Stack,A1),
   addQuestion(Q1,UL,Fecha,TextoPregunta,ListaEtiquetas,Q2),
-  removeLogguedUser(Users,U2),
+  removeLoggedUser(Users,U2),
   stack([U2,Q2,A1],Stack2).
+
+answer(Stack,Fecha,IdPregunta,TextoRespuesta,ListaEtiquetas,Stack2):- getUsers(Stack,Users),
+  validateLoggedUser(Users),
+  getLoggedUser(Users,UL),
+  getQuestions(Stack,Q1),
+  validateQuestionId(Q1,IdPregunta),
+  getAnswers(Stack,A1),
+  addAnswer(A1,IdPregunta,UL,Fecha,TextoRespuesta,ListaEtiquetas,A2),
+  removeLoggedUser(Users,U2),
+  stack([U2,Q1,A2],Stack2).
+
+% EJEMPLO EN CONSOLA:
+% stackRegister([[["Tomas", "#wes@"],["Javiera", "pa$$"]],[],[]],"Fran", "sdad", R), stackLogin(R, "Fran", "sdad", R2), ask(R2, "2020-10-23", "Hola qué tal?", [1,2,45], R3), stackLogin(R3, "Fran", "sdad", R4), ask(R4, "2020-10-23", "Hola quésfasdf tal?", [1,2,45], R5), stackLogin(R5, "Fran", "sdad", R6), answer(R6,"2020-11-28",2,"Tudu bem",["Javascript"], R7), stackLogin(R7, "Javiera", "pa$$", R8),answer(R8,"2020-11-28",1,"bien y túuuuu",["no sé"], R9).
