c69914d2e684a4265a9bc27db645905c6084f44f
javieratapiab <javierafrancisca92@gmail.com>
2020-12-02 23:39:10 -0300

    feat: finaliza predicado vote.

--- main.pl
+++ main.pl
@@ -57,7 +57,7 @@ ask(Stack,Fecha,TextoPregunta,ListaEtiquetas,Stack2):-
   stack(_,_,_,A1,Stack),
   addQuestion(Q1,U0,Fecha,TextoPregunta,ListaEtiquetas,Q2),
   stack([],U1,Q2,A1,Stack2).
-
+ 
 setAnswerId(Answer,NewId):- answer(Id,_,_,_,_,_,_,_,Answer),NewId is Id + 1.
 addAnswer([],QuestionId,[Author|_],Date,Text,Labels,[A]):- answer(1,QuestionId,Author,Date,Text,0,"no",Labels,A).
 addAnswer([H|T],QuestionId,[Author|_],Date,Text,Labels,A):-
@@ -79,20 +79,20 @@ validateAuthorAndQuestion(Questions,QuestionId,[Author|_]):- exists([QuestionId,
 validateAnswerId(Answers,AnswerId):- exists([AnswerId,_,_,_,_,_,_,_],Answers),true.
 
 getQuestion(Stack,QuestionId,Question):-
-  stack(_,_,Questions,_,Stack),
-  find(Questions,[QuestionId,_,_,_,_,_,_],Question).
+    stack(_,_,Questions,_,Stack),
+    find(Questions,[QuestionId,_,_,_,_,_,_],Question).
 
-getAnswer(Stack,AnswerId,Answer):-
-  stack(_,_,_,Answers,Stack),
-  find(Answers,[AnswerId,_,_,_,_,_,_,_],Answer).
+getAnswer(Stack,QuestionId,AnswerId,Answer):- 
+    stack(_,_,_,Answers,Stack),
+    find(Answers,[AnswerId,QuestionId,_,_,_,_,_,_],Answer).
 
 setAnswerAcceptStatus(Answer,UpdatedAnswer):-
-  answer(Id,QuestionId,AnswerAuthor,AnswerDate,AnswerText,AnswerVotes,_,AnswerLabels,Answer),
-  answer(Id,QuestionId,AnswerAuthor,AnswerDate,AnswerText,AnswerVotes,"si",AnswerLabels,UpdatedAnswer).
+ answer(Id,QuestionId,AnswerAuthor,AnswerDate,AnswerText,AnswerVotes,_,AnswerLabels,Answer),
+ answer(Id,QuestionId,AnswerAuthor,AnswerDate,AnswerText,AnswerVotes,"si",AnswerLabels,UpdatedAnswer).
 
 setQuestionAcceptStatus(Question,UpdatedQuestion):-
-  question(Id,QuestionAuthor,QuestionDate,QuestionText,QuestionVotes,_,QuestionLabels,Question),
-  question(Id,QuestionAuthor,QuestionDate,QuestionText,QuestionVotes,"cerrada",QuestionLabels,UpdatedQuestion).
+ question(Id,QuestionAuthor,QuestionDate,QuestionText,QuestionVotes,_,QuestionLabels,Question),
+ question(Id,QuestionAuthor,QuestionDate,QuestionText,QuestionVotes,"cerrada",QuestionLabels,UpdatedQuestion).
 
 updateQuestionAuthorReputation(Users,Question,UpdatedQuestionAuthor):-
   question(_,Author,_,_,_,_,_,Question),
@@ -100,7 +100,7 @@ updateQuestionAuthorReputation(Users,Question,UpdatedQuestionAuthor):-
   user(Username,Password,Reputation,User),
   NewReputation is Reputation + 2,
   user(Username,Password,NewReputation,UpdatedQuestionAuthor).
-
+ 
 updateAnswerAuthorReputation(Users,Answer,UpdatedAnswerAuthor):-
   answer(_,_,Author,_,_,_,_,_,Answer),
   find(Users,[Author,_,_],User),
@@ -112,33 +112,58 @@ updateUsers(Users,UpdatedUser,UpdatedUsers):-
   user(Username,_,_,UpdatedUser),
   find(Users,[Username,_,_],OldUser),
   replace(OldUser,UpdatedUser,Users,UpdatedUsers).
+
 updateQuestions(Questions,UpdatedQuestion,UpdatedQuestions):-
   question(Id,_,_,_,_,_,_,UpdatedQuestion),
   find(Questions,[Id,_,_,_,_,_,_],OldQuestion),
   replace(OldQuestion,UpdatedQuestion,Questions,UpdatedQuestions).
+
 updateAnswers(Answers,UpdatedAnswer,UpdatedAnswers):-
   answer(Id,_,_,_,_,_,_,_,UpdatedAnswer),
   find(Answers,[Id,_,_,_,_,_,_,_],OldAnswer),
   replace(OldAnswer,UpdatedAnswer,Answers,UpdatedAnswers).
 
-accept(Stack,IdPregunta,IdRespuesta,Stack2):- stack(_,U1,_,_,Stack),
-  stack(U0,_,_,_,Stack),
-  lengthList(U0,Length),Length = 1,
-  stack(_,_,Q1,_,Stack),
-  stack(_,_,_,A1,Stack),
-  validateAuthorAndQuestion(Q1,IdPregunta,U0),
-  validateAnswerId(A1,IdRespuesta),
-  getQuestion(Stack,IdPregunta,Question),
-  getAnswer(Stack,IdPregunta,Answer),
-  setQuestionAcceptStatus(Question,UpdatedQuestion),
-  setAnswerAcceptStatus(Answer,UpdatedAnswer),
-  updateQuestionAuthorReputation(U1,UpdatedQuestion,UpdatedQuestionAuthor),
-  updateAnswerAuthorReputation(U1,UpdatedAnswer,UpdatedAnswerAuthor),
-  updateQuestions(Q1,UpdatedQuestion,Q2),
-  updateAnswers(A1,UpdatedAnswer,A2),
-  updateUsers(U1,UpdatedQuestionAuthor,U2),
-  updateUsers(U2,UpdatedAnswerAuthor,U3),
-  stack([],U3,Q2,A2,Stack2).
+accept(Stack,IdPregunta,IdRespuesta,Stack2):-
+ stack(_,U1,_,_,Stack),
+ stack(U0,_,_,_,Stack),
+ lengthList(U0,Length),Length = 1,
+ stack(_,_,Q1,_,Stack),
+ stack(_,_,_,A1,Stack),
+ validateAuthorAndQuestion(Q1,IdPregunta,U0),
+ validateAnswerId(A1,IdRespuesta),
+ getQuestion(Stack,IdPregunta,Question),
+ getAnswer(Stack,IdPregunta,IdRespuesta,Answer),
+ setQuestionAcceptStatus(Question,UpdatedQuestion),
+ setAnswerAcceptStatus(Answer,UpdatedAnswer),
+ updateQuestionAuthorReputation(U1,UpdatedQuestion,UpdatedQuestionAuthor),
+ updateAnswerAuthorReputation(U1,UpdatedAnswer,UpdatedAnswerAuthor),
+ updateQuestions(Q1,UpdatedQuestion,Q2),
+ updateAnswers(A1,UpdatedAnswer,A2),
+ updateUsers(U1,UpdatedQuestionAuthor,U2),
+ updateUsers(U2,UpdatedAnswerAuthor,U3),
+ stack([],U3,Q2,A2,Stack2).
+
+
+%Debe retornar false si el voto va para una pregunta que no es del usuario que tiene sesión activa en el stack.
+setVote(Stack,QuestionOrAnswer,R):-
+ stack(_,_,Q1,_,Stack),
+ find(Q1,QuestionOrAnswer,Question),
+ question(Id,QuestionAuthor,_,_,_,_,_,Question),
+ stack(U0,_,_,_,Stack),
+ U0 = [QuestionAuthor],
+ 
+ 
+ %question(Id,QuestionAuthor,QuestionDate,QuestionText,QuestionVotes,_,QuestionLabels,Question),
+ %stack([],U1,Q1,A1,R).
+
+setVote(Stack,QuestionOrAnswer,R):-
+ stack(_,_,_,A1,Stack),
+ find(A1,QuestionOrAnswer,Answer),
+ stack([],U1,Q1,A1,R).
+
+vote(Stack,TdaPreguntaORespuesta,Voto,Stack2):-
+ setVote(Stack,TdaPreguntaORespuesta,Stack2).
+
 
 /* EJEMPLO EN CONSOLA:
 stackRegister([[],[],[],[]],"Fran", "sdad", R),
